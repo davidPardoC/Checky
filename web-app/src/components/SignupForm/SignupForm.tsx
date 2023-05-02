7aeb3e28fdea057329e03ee336bff0c7be0ed4d0
davidPardoC <pardodavid10@gmail.com>
2023-05-02 00:03:20 -0500

    Add error messages to form

--- web-app/src/components/SignupForm/SignupForm.tsx
+++ web-app/src/components/SignupForm/SignupForm.tsx
@@ -1,24 +1,28 @@
-import {  Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {  Button, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react";
 import Link from "next/link";
 
 export const SignupForm = () => {
   return (
     <form>
-      <FormControl marginTop={3}>
+      <FormControl marginTop={3} isRequired isInvalid>
         <FormLabel>Name</FormLabel>
         <Input type="text" placeholder="Name" />
+        <FormErrorMessage>Name is required</FormErrorMessage>
       </FormControl>
-      <FormControl marginTop={3}>
+      <FormControl marginTop={3} isRequired>
         <FormLabel>Last Name</FormLabel>
         <Input type="text" placeholder="Last Name" />
+        <FormErrorMessage>Name is required</FormErrorMessage>
       </FormControl>
-      <FormControl marginTop={3}>
+      <FormControl marginTop={3} isRequired>
         <FormLabel>Email</FormLabel>
         <Input type="email" placeholder="Email" />
+        <FormErrorMessage>Name is required</FormErrorMessage>
       </FormControl>
-      <FormControl marginTop={3}>
+      <FormControl marginTop={3} isRequired>
         <FormLabel>Password</FormLabel>
         <Input type="Password" placeholder="Password" />
+        <FormErrorMessage>Name is required</FormErrorMessage>
       </FormControl>
       <Link href={"/login"}>
         Already have an account?
